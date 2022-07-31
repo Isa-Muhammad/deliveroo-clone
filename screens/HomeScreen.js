51452493bc2c649653c481be5c6c1360a710ad40
Isa Muhammad <isamuhammadm.im@gmail.com>
2022-07-31 19:25:07 +0100

    Feat: Implement Home screen header & categories section

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,77 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from "react-native-heroicons/outline";
+import Categories from "./components/Categories";
 
 const HomeScreen = () => {
+  //To create a custom header and not use default react native navigation header
+  const navigation = useNavigation(); // Provides acess to navigation object
+
+  useLayoutEffect(() => {
+    // control options using setOptions method
+    navigation.setOptions({
+      headerShown: false,
+      headerTitle: "Testing",
+    });
+  });
   return (
-    <View>
-      <Text className=" text-red-400">HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row  flex-1 space-x-2 bg-gray-200 p-3">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        className="bg-gray-100"
+      >
+        {/* Categories */}
+        <Categories />
+        {/* Feaured Rows */}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
